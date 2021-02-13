1a5f1525da0ee0deacf2508a40cf27d1214b7383
Camoo5 <oluwoleokanlawon91@gmail.com>
2021-02-13 12:18:30 +0000

    Changes to project as of 13 Feb 2021

---
+++ app/src/main/java/com/tenacity/invisibledisabilities/adapters/CriteriaTwoAdapter.java
@@ -0,0 +1,73 @@
+package com.tenacity.invisibledisabilities.adapters;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tenacity.invisibledisabilities.data.CriteriaTwo;
+import com.tenacity.invisibledisabilities.databinding.FragmentCriteriaTwoBinding;
+import com.tenacity.invisibledisabilities.ui.gallery.CriteriaTwoFragmentDirections;
+
+
+public class CriteriaTwoAdapter extends ListAdapter <com.tenacity.invisibledisabilities.data.CriteriaTwo, CriteriaTwoAdapter.ViewHolder> {
+
+    public CriteriaTwoAdapter() {
+        super ( new CriteriaTwoDiffCallback ());
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder ( FragmentCriteriaTwoBinding.inflate (
+                LayoutInflater.from ( parent.getContext () ), parent, false ) );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        com.tenacity.invisibledisabilities.data.CriteriaTwo criteriaTwo = getItem ( position );
+        holder.bind ( createOnClickListener ( criteriaTwo.getCriteriatwoId () ), criteriaTwo );
+        holder.itemView.setTag ( criteriaTwo);
+    }
+
+    private View.OnClickListener createOnClickListener(String criteriatwoId) {
+        return v -> Navigation.findNavController ( v ).navigate (
+                CriteriaTwoFragmentDirections.criteriaTwoFragmentToSubConsiderationsFragment (criteriatwoId ) );
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private final FragmentCriteriaTwoBinding binding;
+
+        ViewHolder(@NonNull FragmentCriteriaTwoBinding binding) {
+            super ( binding.getRoot () );
+            this.binding = binding;
+        }
+
+        void bind(View.OnClickListener listener,   com.tenacity.invisibledisabilities.data.CriteriaTwo item ) {
+            binding.setClickListener(listener);
+            binding.setCriteriatwo ( item );
+            binding.executePendingBindings();
+        }
+    }
+
+    static class CriteriaTwoDiffCallback extends DiffUtil.ItemCallback<CriteriaTwo> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull CriteriaTwo oldItem, @NonNull CriteriaTwo newItem) {
+            return oldItem.getCriteriatwoId ().equals(newItem.getCriteriatwoId ());
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        @Override
+        public boolean areContentsTheSame(@NonNull CriteriaTwo oldItem, @NonNull CriteriaTwo newItem) {
+            return oldItem == newItem;
+        }
+    }
+}
+
---
+++ app/src/main/java/com/tenacity/invisibledisabilities/ui/gallery/CriteriaTwoFragment.java
@@ -0,0 +1,71 @@
+package com.tenacity.invisibledisabilities.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ListAdapter;
+
+import com.tenacity.invisibledisabilities.R;
+import com.tenacity.invisibledisabilities.adapters.CriteriaTwoAdapter;
+import com.tenacity.invisibledisabilities.databinding.FragmentCriteriaTwoBinding;
+import com.tenacity.invisibledisabilities.ui.viewmodels.CriteriaTwoViewModel;
+import com.tenacity.invisibledisabilities.ui.viewmodels.CriteriaTwoViewModelFactory;
+import com.tenacity.invisibledisabilities.utilities.InjectorUtils;
+
+
+public class CriteriaTwoFragment extends Fragment {
+
+    private CriteriaTwoViewModel viewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentCriteriaTwoBinding binding = FragmentCriteriaTwoBinding.inflate(inflater, container, false);
+        CriteriaTwoViewModelFactory factory = InjectorUtils.provideViewModelFactory(getContext());
+        ListAdapter adapter = new CriteriaTwoAdapter ();
+        binding.copingStrategy.setAdapter(adapter);
+        this.viewModel = new ViewModelProvider (this, factory).get(CriteriaTwoViewModel.class);
+        subscribeUi(adapter);
+
+
+        setHasOptionsMenu(true);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate( R.menu.menu_disability_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.filter_criteria:
+                updateData();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void subscribeUi(ListAdapter adapter) {
+        this.viewModel.criteriaone
+
+                .observe(getViewLifecycleOwner(), copingstrategy -> {
+                    if (criteriaone != null) {
+                        adapter.submitList(criteriaone);
+                    }
+                });
+    }
+
+
+}
